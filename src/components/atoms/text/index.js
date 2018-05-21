53e803d69c3bffe40723669bffb91ab8fe902a3a
Frederico Jordan <fredericojordan@gmail.com>
2018-05-21 19:07:06 -0300

    First text atom component

---
+++ src/components/atoms/text/index.js
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+const Text = props => <span>Texto</span>
+
+export default Text
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,19 +1,9 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 
-import logo from '_images/logo.png'
-
-import styles from './styles.css'
+import Text from '_atoms/text'
 
 const CKLBoilerplate = () => (
-  <Fragment>
-    <div className={styles.logoContainer}>
-      <img src={logo} alt="CheesecakeLabs logo" className={styles.logo} />
-    </div>
-    <hr />
-    {/* eslint-disable-next-line jsx-a11y/no-distracting-elements */}
-    <marquee className={styles.text}>React Boilerplate</marquee>
-    <hr />
-  </Fragment>
+    <Text />
 )
 
 export default CKLBoilerplate
